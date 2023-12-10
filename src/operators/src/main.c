2adad1e77f6aff17eeb4d14a5fc8d96df7adf1fa
Marco <marcoguilhermepereira@outlook.com>
2023-12-10 16:39:13 -0300

    Alterações no projeto dos operadores: adição de módulo, diferente, incremento e decremento, atribuição aumentada, operadores lógicos e ternário.

--- src/operators/src/main.c
+++ src/operators/src/main.c
@@ -13,6 +13,7 @@ int main(void) {
     int x3 = x1 + x2;
     // Valor literal na soma
     int x4 = x3 + 63;
+    int result = 0;
 
     printf("x1 + x2 = %i\n\n\n", x3);
 
@@ -32,16 +33,56 @@ int main(void) {
     // decimal
     printf("x1/x2 = %f\n\n\n", x1/(float) x2);
 
+    // Módulo (resto da divisão)
+    printf("83 %% 6 = %d\n\n\n", 83 % 6);
+
     // Precedência dos operadores
     printf("x1 + (x2 * x3) = %i\n", x1 + x2 * x3);
     printf("x1 + (x2 * x3) - (12/x4) = %i\n\n\n", x1 + x2 * x3 - 12/x4);
 
     // Operadores de comparação
     printf("x1 == x2? %d\n", x1 == x2); // Igual
+    printf("x1 != x2? %d\n", x1 == x2); // Diferente
     printf("x1 > x2? %d\n", x1 > x2); // Maior
     printf("x1 < x2? %d\n", x1 < x2); // Menor
     printf("x1 >= x2? %d\n", x1 >= x2); // Maior ou igual
-    printf("x1 <= x2? %d\n", x1 <= x2); // Menor ou igual
+    printf("x1 <= x2? %d\n\n\n", x1 <= x2); // Menor ou igual
+
+    x1 = 4;
+    x2 = 9;
+
+    // Incremento (x1 = x1 + 1)
+    x1++;
+
+    printf("x1++ %d\n", x1);
+
+    // Decremento (x2 = x2 - 1)
+    x2--;
+
+    printf("x2-- %d\n", x2);
+
+    // Primeiro lê o valor da variável, depois incrementa/decrementa
+    printf("x1++ %d\n", x1++);
+    printf("x2-- %d\n", x2--);
+
+    // Primeiro incrementa/decrementa, depois lê o valor da variável
+    printf("++x1 %d\n", ++x1);
+    printf("--x2 %d\n", --x2);
+
+    // Atribuição aumentada
+    x1 += 1;
+    x1 -= 1;
+    x1 *= 2;
+    x1 /= 2;
+    x1 %= 2;
+
+    // Operadores lógicos
+    result = 1 && 0;
+    result = 1 || 0;
+    result = !1;
+
+    // Ternário
+    result = x1 > -5 ? 11 : 22;
 
     return 0;
 }
